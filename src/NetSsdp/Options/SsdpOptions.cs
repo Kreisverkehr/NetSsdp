e0bab94c4a0903227c5783643efee54fe61e8610
Kreisverkehr <mm.rueter@gmail.com>
2026-06-23 20:01:01 +0200

    auto configure protocols

--- src/NetSsdp/Options/SsdpOptions.cs
+++ src/NetSsdp/Options/SsdpOptions.cs
@@ -1,8 +1,10 @@
+using System.Net.Sockets;
+
 namespace Kreisverkehr.NetSsdp.Options;
 
 public class SsdpOptions
 {
-    public bool UseIPv4 { get; set; } = true;
-    public bool UseIPv6 { get; set; } = true;
+    public bool UseIPv4 { get; set; } = Socket.OSSupportsIPv4;
+    public bool UseIPv6 { get; set; } = Socket.OSSupportsIPv6;
 
 }
